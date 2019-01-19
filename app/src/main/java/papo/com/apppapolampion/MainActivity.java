aacd5e738537bf3ffd27196f0526989c9cc78631
Rousseau Romain <romain.rousseau5@gmail.com>
2019-01-20 00:19:52 +0100

    Adding redirection to menu buttons

--- app/src/main/java/papo/com/apppapolampion/MainActivity.java
+++ app/src/main/java/papo/com/apppapolampion/MainActivity.java
@@ -3,18 +3,40 @@ package papo.com.apppapolampion;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        startActivity(this.getIntent());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
+    public void startProgramActivity(View view)
+    {
+        Intent intent = new Intent(this, program_add_event.class);
+        startActivity(intent);
+    }
+
+    public void startChangeColorActivity(View view)
+    {
+        Intent intent = new Intent(this, pick_color.class);
+        startActivity(intent);
+    }
+    
+    public void startTurnOnActivity(View view)
+    {
+        Intent intent = new Intent(this, program_select_laumios.class);
+        startActivity(intent);
+    }
+    public void startChangeAnimationActivity(View view)
+    {
+        Intent intent = new Intent(this, select_animation.class);
+        startActivity(intent);
+    }
     /*
     @Override
     protected void onResume() {
