d2cb9d34aa1096de0b29e93b36afd3eb496bc616
Unknown <dlpsandra31@gmail.com>
2019-01-19 22:14:41 +0100

    add color wheel

--- app/src/main/java/papo/com/apppapolampion/pick_color.java
+++ app/src/main/java/papo/com/apppapolampion/pick_color.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import top.defaults.colorpicker.ColorPickerPopup;
+
 public class pick_color extends AppCompatActivity {
 
 
@@ -15,6 +17,23 @@ public class pick_color extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final View v = findViewById(R.id.v);
+        new ColorPickerPopup.Builder(this)
+                .initialColor(Color.RED) // Set initial color
+                .enableBrightness(true) // Enable brightness slider or not
+                .enableAlpha(true) // Enable alpha slider or not
+                .okTitle("Choose")
+                .cancelTitle("Cancel")
+                .showIndicator(true)
+                .showValue(true)
+                .build()
+                .show(v, new ColorPickerPopup.ColorPickerObserver() {
+                    @Override
+                    public void onColorPicked(int color) {
+                        v.setBackgroundColor(color);
+                    }
+
+                });
 
     }
 
