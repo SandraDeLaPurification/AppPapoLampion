89cbdc6ddec4a08babe5d06fe41070f7d0ade849
Unknown <dlpsandra31@gmail.com>
2019-01-19 23:32:19 +0100

    color picker

--- app/src/main/java/papo/com/apppapolampion/MainActivity.java
+++ app/src/main/java/papo/com/apppapolampion/MainActivity.java
@@ -1,5 +1,6 @@
 package papo.com.apppapolampion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +11,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = new Intent(MainActivity.this, pick_color.class);
+        startActivity(intent);
+    }
+    */
 }
--- app/src/main/java/papo/com/apppapolampion/pick_color.java
+++ app/src/main/java/papo/com/apppapolampion/pick_color.java
@@ -2,38 +2,32 @@ package papo.com.apppapolampion;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import top.defaults.colorpicker.ColorObserver;
 import top.defaults.colorpicker.ColorPickerPopup;
+import top.defaults.colorpicker.ColorPickerView;
 
 public class pick_color extends AppCompatActivity {
 
+    private static final int INITIAL_COLOR = 0xFFFF8000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final View v = findViewById(R.id.v);
-        new ColorPickerPopup.Builder(this)
-                .initialColor(Color.RED) // Set initial color
-                .enableBrightness(true) // Enable brightness slider or not
-                .enableAlpha(true) // Enable alpha slider or not
-                .okTitle("Choose")
-                .cancelTitle("Cancel")
-                .showIndicator(true)
-                .showValue(true)
-                .build()
-                .show(v, new ColorPickerPopup.ColorPickerObserver() {
-                    @Override
-                    public void onColorPicked(int color) {
-                        v.setBackgroundColor(color);
-                    }
+        setContentView(R.layout.activity_pick_color);
 
-                });
+        final View v = findViewById(R.id.v);
+
+        ColorPickerView colorPickerView = findViewById(R.id.colorPickerView);
+
 
     }
 
