2799267496423e6dfcb70a6260b931c9b7ce243c
Unknown <dlpsandra31@gmail.com>
2019-01-19 22:04:27 +0100

    gradle de merde

--- app/src/main/java/papo/com/apppapolampion/pick_color.java
+++ app/src/main/java/papo/com/apppapolampion/pick_color.java
@@ -1,13 +1,21 @@
 package papo.com.apppapolampion;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class pick_color extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pick_color);
+        setContentView(R.layout.activity_main);
+
     }
+
 }
